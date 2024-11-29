38c019b26da0d28ed20b3f554303480cd6b21f9c
Stefan Brown <stefanrbrown11@gmail.com>
2024-11-29 13:19:22 +0000

    article id comments refactor

--- models/api.model.js
+++ models/api.model.js
@@ -124,17 +124,6 @@ function fetchArticleComments(article_id) {
     })
 }
 
-/* const queryString = `
-    SELECT comment_id, votes, created_at, author, body, article_id
-    FROM comments
-    WHERE article_id = $1
-    ORDER BY created_at DESC;
-  `
-
-  return db.query(queryString, [article_id]).then(({ rows }) => {
-    return rows
-  }) */
-
 function insertArticleComment(article_id, username, body) {
   const queryString = `INSERT INTO comments (article_id, author, body)
     VALUES ($1, $2, $3)
